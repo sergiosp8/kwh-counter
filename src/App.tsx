49d7598f9f18bed1ed10e069144d1da4263b068c
Sergio Sosa <sergiosp8@gmail.com>
2022-06-08 18:24:56 -0500

    se agrego la logica para el calculo de kwhpromedio y kwhconsumido

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,19 @@ dayjs.locale(es)
 
 function App() {
   const [fechaCorte, setFechaCorte] = useState<dayjs.Dayjs | null>(null)
-  const [kwhcorte, setKwhCorte] = useState(0)
+  const [kwhCorte, setKwhCorte] = useState(0)
   const [kwhActual, setKwhActual] = useState(0)
   const [mensual, setMensual] = useState(false)
   const [bimestral, setBimestral] = useState(false)
   const [isMensualActivo, setIsMensualActivo] = useState(true)
   const [isBimestralActivo, setIsBimestralActivo] = useState(true)
   const [diasTranscurridos, setDiasTranscurridos] = useState('')
+  const [kwhConsumidos, setKwhConsumidos] = useState('')
+  const [kwhPromedio, setKwhPromedio] = useState('')
+
   const handleChangeInputDate = (event: React.ChangeEvent<HTMLInputElement>) => {
     const temp = dayjs(event.target.value)
     setFechaCorte(temp)
-    const tempDiasTrans = calcularDiasTranscurridosAlDiaDeHoy(temp)
-    setDiasTranscurridos(tempDiasTrans)
   }
 
   const handleChangeInputText = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,9 +46,30 @@ function App() {
     }
   }
 
+  const handleSubmitCalcular = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    let diasDelPeriodo = 0
+    if (mensual) {
+      diasDelPeriodo = 30
+    }
+    if (bimestral) {
+      diasDelPeriodo = 60
+    }
+
+    const kwhConsumidosTemp = kwhActual - kwhCorte
+    console.log(kwhConsumidosTemp)
+    const tempDiasTrans = calcularDiasTranscurridosAlDiaDeHoy(fechaCorte as dayjs.Dayjs)
+    setDiasTranscurridos(tempDiasTrans)
+
+    setKwhConsumidos(kwhConsumidosTemp.toString())
+
+    const tempKwhPromedio = kwhConsumidosTemp / diasDelPeriodo
+    setKwhPromedio(tempKwhPromedio.toFixed(2).toString())
+  }
+
   const calcularDiasTranscurridosAlDiaDeHoy = (fecha: dayjs.Dayjs): string => {
     const fechaActual = dayjs()
-    console.log(fechaActual)
     const diasTranscurridos = fechaActual.diff(fecha.format('YYYY-MM-DD'), 'days')
     return diasTranscurridos.toString()
   }
@@ -55,7 +77,7 @@ function App() {
   return (
     <div className="App">
       <h1>Kwh Counter 💡👌</h1>
-      <form>
+      <form onSubmit={handleSubmitCalcular}>
         <label htmlFor="fechacorte">Fecha de corte : </label>
         <input onChange={handleChangeInputDate} type="date" name="fechacorte" />
         <label>
@@ -68,14 +90,14 @@ function App() {
           <input onChange={handleChangeInputCheckbox} type="checkbox" name="bimestral" disabled={!isBimestralActivo} />
         </label>
         <label htmlFor="kwhcorte">Kwh al corte : </label>
-        <input onChange={handleChangeInputText} type="number" name="kwhcorte" value={kwhcorte} />
+        <input onChange={handleChangeInputText} type="number" name="kwhcorte" value={kwhCorte} />
         <label htmlFor="kwhactual">Kwh actuales : </label>
         <input onChange={handleChangeInputText} type="number" name="kwhactual" value={kwhActual} />
         <button>Calcular</button>
       </form>
       <p>Dias Trabscurridos : {diasTranscurridos}</p>
-      <p>Kwh consumidos : 580 kwh</p>
-      <p>Kwh promedio diarios : 18.57 kwh</p>
+      <p>Kwh consumidos : {kwhConsumidos} kwh</p>
+      <p>Kwh promedio diarios : {kwhPromedio} kwh</p>
     </div>
   )
 }
