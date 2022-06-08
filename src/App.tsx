6de4067a797f7d940d17924bccd9607b9f6524c9
Sergio Sosa <sergiosp8@gmail.com>
2022-06-07 20:20:44 -0500

    se agrago la funcionalidad de los checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [fechacorte, setFechaCorte] = useState<Date | null>(null)
+  const [fechaCorte, setFechaCorte] = useState<Date | null>(null)
   const [kwhcorte, setKwhCorte] = useState(0)
-  const [kwhactual, setKwhActual] = useState(0)
+  const [kwhActual, setKwhActual] = useState(0)
   const [mensual, setMensual] = useState(false)
   const [bimestral, setBimestral] = useState(false)
+  const [isMensualActivo, setIsMensualActivo] = useState(true)
+  const [isBimestralActivo, setIsBimestralActivo] = useState(true)
 
   const handleChangeInputDate = (event: React.ChangeEvent<HTMLInputElement>) => {
     const tempDate = new Date(event.target.value + 'T00:00:00')
@@ -27,10 +29,14 @@ function App() {
     const { name, checked } = event.target
     if (name === 'mensual') {
       setMensual(checked)
+      setIsMensualActivo(true)
+      setIsBimestralActivo(!isBimestralActivo)
     }
 
     if (name === 'bimestral') {
       setBimestral(checked)
+      setIsBimestralActivo(true)
+      setIsMensualActivo(!isMensualActivo)
     }
   }
 
@@ -42,17 +48,17 @@ function App() {
         <input onChange={handleChangeInputDate} type="date" name="fechacorte" />
         <label>
           Mensual :
-          <input onChange={handleChangeInputCheckbox} type="checkbox" name="mensual" />
+          <input onChange={handleChangeInputCheckbox} type="checkbox" name="mensual" disabled={!isMensualActivo} />
         </label>
 
         <label>
           Bimestral :
-          <input onChange={handleChangeInputCheckbox} type="checkbox" name="bimestral" />
+          <input onChange={handleChangeInputCheckbox} type="checkbox" name="bimestral" disabled={!isBimestralActivo} />
         </label>
         <label htmlFor="kwhcorte">Kwh al corte : </label>
         <input onChange={handleChangeInputText} type="number" name="kwhcorte" value={kwhcorte} />
         <label htmlFor="kwhactual">Kwh actuales : </label>
-        <input onChange={handleChangeInputText} type="number" name="kwhactual" value={kwhactual} />
+        <input onChange={handleChangeInputText} type="number" name="kwhactual" value={kwhActual} />
         <button>Calcular</button>
       </form>
       <p>Dias Trabscurridos : 15</p>
