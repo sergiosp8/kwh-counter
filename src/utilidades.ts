d1e8451010f317a4a0024d14ab83e03031e16bc7
Sergio Sosa <sergiosp8@gmail.com>
2022-06-09 19:53:22 -0500

    se agrego un archivo externo de utilidade para simplificar funciones con, la libreria days.js

--- src/utilidades.ts
+++ src/utilidades.ts
@@ -8,3 +8,12 @@ export const diasTranscurridosAlDiaDeHoy = (fecha: dayjs.Dayjs): string => {
   const diasTranscurridos = fechaActual.diff(fecha.format('YYYY-MM-DD'), 'days')
   return diasTranscurridos.toString()
 }
+
+export const obtenerFecha = (fecha: string): dayjs.Dayjs => {
+  return dayjs(fecha)
+}
+
+export const fechaMayorQueHoy = (fecha: dayjs.Dayjs): boolean => {
+  const fechaActual = dayjs()
+  return fecha.isAfter(fechaActual)
+}
