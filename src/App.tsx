d21976b249b2ef0f48e001edb832c3b91c4fc366
Sergio Sosa <sergiosp8@gmail.com>
2022-06-09 19:52:04 -0500

    se simplificaron los estados y se agrego un objeto de para incicializar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import FormKwhCounter from './components/kwhcounter/FormKwhCounter'
+import { KwhCounterEntidad } from '././components/kwhcounter/types'
+import { useState } from 'react'
+
+const INITAL_STATE: KwhCounterEntidad = {
+  fechaCorte: null,
+  modoCobro: 'mensual',
+  kwhCorte: '',
+  kwhActual: ''
+}
+
 function App() {
-  const [diasTranscurridos, setDiasTranscurridos] = useState(0)
-  const [kwhConsumidos, setKwhConsumidos] = useState('')
-  const [kwhPromedio, setKwhPromedio] = useState('')
-  const [diasTerminoPeriodo, setDiasTerminoPeriodo] = useState(0)
+  const [kwhCounterEntidad, setKwhCounterEntidad] = useState<KwhCounterEntidad>(INITAL_STATE)
 
   return (
     <div className="App">
       <h1>Kwh Counter 💡👌</h1>
-
-      <p>Dias Trabscurridos : {diasTranscurridos}</p>
+      <FormKwhCounter />
+      {/* <p>Dias Trabscurridos : {diasTranscurridos}</p>
       <p>Kwh consumidos : {kwhConsumidos} kwh</p>
       <p>Kwh promedio diarios : {kwhPromedio} kwh</p>
-      <p>Dias para terminar este periodo : {diasTerminoPeriodo} días</p>
+      <p>Dias para terminar este periodo : {diasTerminoPeriodo} días</p> */}
     </div>
   )
 }
