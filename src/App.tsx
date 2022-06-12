bb3407c971d8a75595bd351eb35d36ffdfd7fc6a
Sergio Sosa <sergiosp8@gmail.com>
2022-06-12 15:35:24 -0500

    cambios en el app para simplificar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import FormKwhCounter from './components/kwhcounter/FormKwhCounter'
 import { KwhCounterEntidad } from '././components/kwhcounter/types'
 import { useState } from 'react'
+import CardKwhCounter from './components/kwhcounter/CardKwhCounter'
 
 const INITAL_STATE: KwhCounterEntidad = {
   fechaCorte: null,
   modoCobro: 'mensual',
-  kwhCorte: '',
-  kwhActual: ''
+  kwhCorte: 0,
+  kwhActual: 0
 }
 
 function App() {
   const [kwhCounterEntidad, setKwhCounterEntidad] = useState<KwhCounterEntidad>(INITAL_STATE)
 
+  const actualizarKwhCounterEntidad = (kwhCounterEntidad: KwhCounterEntidad) => {
+    setKwhCounterEntidad(kwhCounterEntidad)
+  }
+
   return (
     <div className="App">
       <h1>Kwh Counter 💡👌</h1>
-      <FormKwhCounter />
+      {JSON.stringify(kwhCounterEntidad)}
+      <FormKwhCounter actualizarKwhCounterEntidad={actualizarKwhCounterEntidad} />
+      {kwhCounterEntidad.fechaCorte && <CardKwhCounter />}
       {/* <p>Dias Trabscurridos : {diasTranscurridos}</p>
       <p>Kwh consumidos : {kwhConsumidos} kwh</p>
       <p>Kwh promedio diarios : {kwhPromedio} kwh</p>
